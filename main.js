fc4e2a10a907d2cd012a900c0f5cae0903605e09
yhy7805 <yhy7805@gmail.com>
2023-09-20 16:07:18 +0900

    현재 시간에 따라 PM AM 추가

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
-//외부스크립트 파일을 head 안 쪽에서 defer 방식으로 출력
-//main.js 해석과 DOM 렌더링을 병렬식으로 동시에 실행
-//해석은 동시에 하지만 실제 호출은 화면의 렌더링이 완료시 호출
-//type= module 적용하면 위의 기능과 전역변수의 모둘화를 한번에 처리
-const box = document.querySelector('article');
-console.log(box)
+/*
+객체 생성하는 방법
+-객체 리터럴 통한 생성
+- 생성자 함수 통한 생성 (new 키워드 이용해서 동일한 구조의 객체를 복사할 때 주로 사용)
+- 생성자함수 통해서 생성된 객체 : 인스턴스
+-인스턴스 객체에는 prototype이라는 공통의 저장공간이 포함되어있어 자주쓰는 함수 등록후 자유롭게 사용가능
+*/
+
+const now = new Date();
+console.dir(now);
+
+//우리나라 시간대는 전게셰 표준 시간대에서 9시간이 더해진 시간 (그리니치 표준 시간대 적용)
+
+const year = now.getFullYear();
+console.log(year)
+
+const months = ['january', 'febuary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'decemmber'];
+
+const month = now.getMonth(); // 순번을 반환 
+console.log(months[month]);
+
+const date = now.getDate();
+console.log(date);
+
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+const day = now.getDay();
+console.log(days[day]);
+
+const min = now.getMinutes();
+console.log(min);
+
+const sec = now.getSeconds();
+console.log(sec);
+
+
+
+console.log(hours);
+
+let newHr = 0;
+
+if(hours >12){
+    newHr = hours -12 + 'PM';
+}else{
+    newHr = hours + 'AM';
+}
+
+// const hours = now.getHours()
+//     if(hours>=12){
+//         console.log(`${hours -12}PM`)
+//     }
