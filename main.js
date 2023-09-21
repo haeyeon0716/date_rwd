121ac94f412e980cdad69e982b7f4cd7862b16d4
yhy7805 <yhy7805@gmail.com>
2023-09-21 09:36:51 +0900

    시간에 따른 단위 변화 조건식 축약 삼항, &&

--- main.js
+++ main.js
@@ -1,58 +1,45 @@
-/*
-객체 생성하는 방법
--객체 리터럴 통한 생성
-- 생성자 함수 통한 생성 (new 키워드 이용해서 동일한 구조의 객체를 복사할 때 주로 사용)
-- 생성자함수 통해서 생성된 객체 : 인스턴스
--인스턴스 객체에는 prototype이라는 공통의 저장공간이 포함되어있어 자주쓰는 함수 등록후 자유롭게 사용가능
-*/
-
 const now = new Date();
-console.dir(now);
-
-//우리나라 시간대는 전게셰 표준 시간대에서 9시간이 더해진 시간 (그리니치 표준 시간대 적용)
-
-const year = now.getFullYear();
-console.log(year)
-
-const months = ['january', 'febuary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'decemmber'];
-
-const month = now.getMonth(); // 순번을 반환 
-console.log(months[month]);
-
-const date = now.getDate();
-console.log(date);
-
-const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-const day = now.getDay();
-console.log(days[day]);
-
 const min = now.getMinutes();
-console.log(min);
-
 const sec = now.getSeconds();
-console.log(sec);
+const hours = now.getHours();
 
-const hours = now.getHours()
-console.log(hours)
-let newHr = 0;
+console.log(setNumbers(hours));
 
-if(hours >12){
-    newHr = hours -12;
-    if(newHr < 10){
-        newHr = '0' + newHr + 'PM'
-    }
-    else{
-        newHr = newHr + 'PM'
-    }
+function setNumbers(num) {
+    let result = 0;
+    /*
+    if(num<10) num ='0' + num;
+    */
+    //조건식 ? true일 때 실행할 구문 : false일 때 실행할 구문
+    //삼항연산자 구문안에는 대입연산자가 들어올 수 없기 때문에 괄호로 묶어 표현식으로 변경
+    //특정 값이 else일 때 굳이 실행할 코드가 없다면 &&연산자 사용
+    // (num < 10) ? (result ='0' + num) : (result = num);
+
+    num < 10 && (num = '0' + num);
+    return 
+
 }
-else{
-    newHr = hours;
 
-    if(newHr < 10){
-        newHr = '0' + newHr + 'AM'
-    }
-    else{
+/*
+ let newHr = 0;
+
+ if(hours >12){
+     newHr = hours -12;
+     if(newHr < 10){
+         newHr = '0' + newHr + 'PM'
+     }
+     else{
+         newHr = newHr + 'PM'
+     }
+ }
+ else{
+     newHr = hours;
+
+     if(newHr < 10){
+         newHr = '0' + newHr + 'AM'
+     }
+     else{
         newHr = newHr + 'AM'
-    }
-}
-console.log(newHr);
+     }
+ }
+ */
