01df68c385f3620a7ff5e853a8d9c7fa44fa9a9b
yhy7805 <yhy7805@gmail.com>
2023-09-21 10:17:31 +0900

    시간값 가공

--- main.js
+++ main.js
@@ -1,45 +1,22 @@
+const [hr, m, s] = document.querySelectorAll('h1 span');
+
 const now = new Date();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 const hours = now.getHours();
 
-console.log(setNumbers(hours));
+//hours 값은 setHours가 조건에 12를 뺀 값으로 반환
+//반환된 결과값을 다시 바로 setNumbers에 집어넣어서 0을 붙이도록 처리
+hr.innerText = setNumbers(setHours(hours));
+m.innerText = setNumbers(min);
+s.innerText = setNumbers(sec);
 
 function setNumbers(num) {
-    let result = 0;
-    /*
-    if(num<10) num ='0' + num;
-    */
-    //조건식 ? true일 때 실행할 구문 : false일 때 실행할 구문
-    //삼항연산자 구문안에는 대입연산자가 들어올 수 없기 때문에 괄호로 묶어 표현식으로 변경
-    //특정 값이 else일 때 굳이 실행할 코드가 없다면 &&연산자 사용
-    // (num < 10) ? (result ='0' + num) : (result = num);
-
     num < 10 && (num = '0' + num);
-    return 
-
+    return num;
 }
 
-/*
- let newHr = 0;
-
- if(hours >12){
-     newHr = hours -12;
-     if(newHr < 10){
-         newHr = '0' + newHr + 'PM'
-     }
-     else{
-         newHr = newHr + 'PM'
-     }
- }
- else{
-     newHr = hours;
-
-     if(newHr < 10){
-         newHr = '0' + newHr + 'AM'
-     }
-     else{
-        newHr = newHr + 'AM'
-     }
- }
- */
+function setHours(num){
+    num > 12 && (num = num-12);
+    return num;
+}
